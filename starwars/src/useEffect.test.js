6c93d5c7de7d223172b28a9715ea6ad9fe97f9a5
Francis Bulus <notchera@gmail.com>
2019-09-13 10:34:59 +0100

    installed axios-mock-adapter for test

---
+++ starwars/src/useEffect.test.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { renderHook, act } from '@testing-library/react-hooks'; 
+
+test ("useEffect performs GET request", () => {
+    const initialValue = [];
+    const mock = new MockAdapter(axios)
+}
+
+)
