611debc97a55ec0a8045fc3eb37e8b915147084f
Francis Bulus <notchera@gmail.com>
2019-09-13 08:52:17 +0100

    built out Characters component and exported it

--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -1,2 +1,25 @@
 import React from "react";
 import Character from "./Character";
+
+const Characters = ({ characters }) => {
+    return (
+      <div className="characters-container">
+        {characters.map((character, index) => {
+          return (
+            <Character
+              key={index}
+              name={character.name}
+              gender = {character.gender}
+              height={character.height}
+              mass={character.mass}
+              hairColor={character.hair_color}
+              skinColor={character.skin_color}
+              birthYear={character.birth_year}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+  
+  export default Characters
