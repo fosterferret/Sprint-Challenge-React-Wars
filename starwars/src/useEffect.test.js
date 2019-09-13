2c87ce82a1fe288347f01c714a30356b2b467a42
Francis Bulus <notchera@gmail.com>
2019-09-13 11:14:05 +0100

    modified tests

--- starwars/src/useEffect.test.js
+++ starwars/src/useEffect.test.js
@@ -9,10 +9,10 @@ test("useEffect performs GET request", () => {
 
   const mockData = "response";
   const url = `https://swapi.co/api/people?page=${currentPage}`;
-  mock.onGet(url).reply(200, mockData);
+  
 
   act(() => {
-    mock.get(url);
+    mock.onGet(url).reply(200, mockData);
   });
 
   expect(mockData.data.results).toBe(initialValue.length > 1);
