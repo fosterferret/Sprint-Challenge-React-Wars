cfbadf21339df1f4d61285df8d5e9ade4f3ce1b7
Francis Bulus <notchera@gmail.com>
2019-09-13 08:44:08 +0100

    installed axios and imported axios, useEffect and useState into App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import "./App.css";
 import './App.css';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    if (currentPage < 10) {
+      axios
+        .get(`https://swapi.co/api/people?page=${currentPage}`)
+        .then(response => {
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [currentPage]);
 
 
   return (
